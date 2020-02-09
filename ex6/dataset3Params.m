1d5cd8c41138c920e170e88c9be6f593eaef7518
Tigran Margarian <44804755+tmargarian@users.noreply.github.com>
2020-02-08 22:09:04 -0800

    ex6 (SVM) completed

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -8,8 +8,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+C_ar = [0.01, 0,03, 0.1, 0.3, 1, 3, 10, 30];
+sigma_ar = [0.01, 0,03, 0.1, 0.3, 1, 3, 10, 30];
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,11 +23,20 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+er = zeros(length(C_ar), length(sigma_ar));
 
+for i = 1:length(C_ar)
+    for j = 1:length(sigma_ar)
+        model = svmTrain(X, y, C_ar(i), @(x1, x2)gaussianKernel(x1, x2, sigma_ar(j)));
+        er(i,j) = mean(double(svmPredict(model, Xval) ~= yval));
+    end
+end
 
+minimum = min(min(er));
+[a, b] = find(er == minimum);
 
-
-
+C = C_ar(a);
+sigma = sigma_ar(b);
 
 % =========================================================================
 
